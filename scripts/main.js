0cdbcb47cf364bd0763c9b4d1051e150d07694c3
asdwqs <1578047010@QQ.COM>
2025-03-22 22:21:43 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+document.querySelector("html").addEventListener("click", function () {
+    alert("别戳我，我怕疼！");
+  });
+  
